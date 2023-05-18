ddaaff2d2b8e83241a39fea623e8062dd764161f
plas318 <plas3180g@gmail.com>
2023-05-18 17:53:13 +0900

    feat: initialized drf-spectacular(swagger), update requirements.txt

--- purgo_back/settings.py
+++ purgo_back/settings.py
@@ -170,6 +170,17 @@ REST_FRAMEWORK = {
 
     # Permission options
     'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated'],
+
+    # DRF Spectacular (Swagger openapi)
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
 
 LOGGING = {
